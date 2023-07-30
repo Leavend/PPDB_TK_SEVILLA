05894f31101151000c2ce2354986a7bda43bc24d
Leavend <tiohadybayu@gmail.com>
2023-07-31 01:48:20 +0800

    edit yml

--- deployer.sh
+++ deployer.sh
@@ -1,12 +1,45 @@
-set -e
+#!/bin/bash
 
-echo 'Deploying...'
+# Hentikan web server atau worker jika diperlukan
+# Contoh: systemctl stop nginx
+# Contoh: supervisorctl stop laravel-worker
 
-(php artisan down --message 'The App is being (Quickly!), Maintenance')
+# Masuk ke direktori proyek Laravel
+# cd /var/www/PPDB_TK_SEVILLA
 
-    git pull origin Version5
+# Pastikan Anda berada di branch yang benar (misalnya, "Version5")
+sudo git checkout Version5
 
-php artisan up
+# Tarik perubahan terbaru dari repositori remote
+sudo git pull origin Version5
 
+# Install dependensi PHP (misalnya, dengan Composer)
+sudo composer install --no-dev --optimize-autoloader
+
+# Hapus cache Laravel jika diperlukan
+# php artisan cache:clear
+
+# Hapus file cache view jika diperlukan
+# php artisan view:clear
+
+# Jalankan migrasi database jika diperlukan
+# php artisan migrate --force
+
+# Generate kunci aplikasi (jika belum ada)
+# php artisan key:generate
+
+# Setel ulang cache konfigurasi (jika diperlukan)
+# php artisan config:cache
+
+# Setel ulang cache route (jika diperlukan)
+# php artisan route:cache
+
+# Setel ulang cache view (jika diperlukan)
+# php artisan view:cache
+
+# Pastikan web server atau worker berjalan kembali
+# Contoh: systemctl start nginx
+# Contoh: supervisorctl start laravel-worker
+
+# Tampilkan pesan deploy selesai
 echo 'Deployed'
-
